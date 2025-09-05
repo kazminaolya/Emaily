a6c79e42ee1971d550744b218ee33df0806bb49e
olga <olga.kazmina@nextgenclearing.com>
2025-09-05 13:24:09 +0300

    add Google oauth, add lints, do refactor

---
+++ eslint.config.mjs
@@ -0,0 +1,42 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import { defineConfig } from 'eslint/config';
+
+export default defineConfig([
+	{
+		files: ['**/*.{js,mjs,cjs}'],
+		plugins: { js },
+		extends: ['js/recommended'],
+		languageOptions: { globals: {...globals.node }},
+	},
+	{
+		ignores: ['node_modules/', 'build/', 'coverage/', 'public/'],
+		rules: {
+			'no-unused-vars': 'warn',
+			'no-undef': 'warn',
+			'no-console': 'warn',
+			'no-debugger': 'warn',
+			'no-empty': 'warn',
+			'no-extra-semi': 'warn',
+			'no-irregular-whitespace': 'warn',
+			'no-unreachable': 'warn',
+			'no-unused-expressions': 'warn',
+			'indent': [
+				'error',
+				'tab'
+			],
+			'linebreak-style': [
+				'error',
+				'unix'
+			],
+			'quotes': [
+				'error',
+				'single'
+			],
+			'semi': [
+				'error',
+				'always'
+			]
+		}
+	}
+]);
