97f191c9c7a54d139a5c14dbe3bec56d783ecd53
olga <olga.kazmina@nextgenclearing.com>
2025-11-05 18:10:52 +0200

    fix routing in production

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
-if(process.env.NODE_ENV !== 'production') {
+if(process.env.NODE_ENV === 'production') {
   // Express will serve up production assets
   // like our main.js file, or main.css file!
   app.use(express.static('client/build'));
@@ -35,7 +35,7 @@ if(process.env.NODE_ENV !== 'production') {
   // if it doesn't recognize the route
   const path = require('path');
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/build/index.html'));
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
   })
 }
 
