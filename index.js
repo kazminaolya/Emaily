d98fd938c911eaabd95ee5a48c23d69ff4a934ee
olga <olga.kazmina@nextgenclearing.com>
2025-11-05 20:00:30 +0200

    fix routing in production

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ if(process.env.NODE_ENV === 'production') {
   const path = require('path');
   // Express will serve up production assets
   // like our main.js file, or main.css file!
-  app.use(express.static(path.join(__dirname, 'client', 'build')));
+  app.use(express.static('client/build'));
 
   // Express will serve up the index.html file
   // if it doesn't recognize the route
